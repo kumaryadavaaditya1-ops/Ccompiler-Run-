556c5f30883ae6e4b0faed1c676c10e3860396bd
kumaryadavaaditya1-ops <kumaryadavaaditya1@gmail.com>
2026-09-18 13:00:05 +0545

    Create script.js
    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function runCode() {
+    const code = document.getElementById("code").value;
+
+    document.getElementById("output").innerText =
+        "Your code has been received!\n\n" + code;
+}
